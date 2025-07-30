c6fba08a9795374bd58a8d4e5cb857624df76fbb
joao_junio <joaojunio818@gmail.com>
2025-07-29 22:08:41 -0300

    Alinhando itens na view com o layout

--- conversor/lib/app/views/home_view.dart
+++ conversor/lib/app/views/home_view.dart
@@ -4,40 +4,74 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(500),
-              child: Image.network(
-                 'https://st2.depositphotos.com/1185628/11062/v/450/depositphotos_110627010-stock-illustration-currency-converter-exchange-icon.jpg',
-              )
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: DropdownButton(
-                      items: [], 
-                      onChanged: (value) {}
-                    )
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(),
-                    )
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(500),
+                child: Image.network(
+                   'https://st2.depositphotos.com/1185628/11062/v/450/depositphotos_110627010-stock-illustration-currency-converter-exchange-icon.jpg',
+                   width: 150, 
+                   height: 150,
+                )
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {}, 
-              child: Text('CONVERTER')
-            ),
-          ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 63,
+                        child: DropdownButton(
+                          isExpanded: true,
+                          underline: Container(
+                            height: 1, 
+                            color: Colors.amber
+                          ),
+                          items: [
+                            DropdownMenuItem(
+                              value: 'Real',
+                              child: Text('Real'),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Dolar',
+                              child: Text('Dolar'),
+                            ),
+                          ], 
+                          onChanged: (value) {}
+                        )
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber)
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber)
+                          ),
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                ),
+                onPressed: () {}, 
+                child: Text('CONVERTER', style: TextStyle(color: Colors.white),)
+              ),
+            ],
+          ),
         ),
       )
     );
